1b459e23bf5a0c16fd70d9f38fbd8f7efa40b866
Douglas <hfsdouglas@gmail.com>
2022-09-11 09:24:10 -0300

    Estilização com flexbox no index

--- index.php
+++ index.php
@@ -9,16 +9,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/index.css">
     <title>Previsão do Tempo</title>
 </head>
 <body>
-    <h1>Saiba a previsão do tempo na sua cidade!</h1>
-    <form action="weather.php" method="post">
-        <input type="text" name="city">
-        <select name="state" id="">
-            <?=$states->get_states()?>
-        </select>
-        <button type="submit">Pesquisar</button>
-    </form>
+    <div class="container">
+        <div class="box">
+            <h1>Saiba a previsão do tempo na sua cidade!</h1>
+            <form action="weather.php" method="post">
+                <input type="text" name="city">
+                <select name="state" id="">
+                    <?=$states->get_states()?>
+                </select>
+            <button type="submit">Pesquisar</button>
+            </form>
+        </div>
+    </div>
+    
 </body>
 </html>
