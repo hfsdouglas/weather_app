2bd9c34ede2f2feecc4ff452607ec508f584650c
Douglas Faria <douglas.faria@proxyerp.com.br>
2023-04-12 23:08:53 -0300

    update: realizando a leitura vindo do serviço com o fetch

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,44 @@
-const optionState = document.getElementById('states')
-const formCities = document.getElementById('cities')
+var estados_select = document.querySelector('.estados');
+var cidades_select = document.querySelector('.cidades');
 
-function loadCities() {
-    let id = optionState.options[optionState.selectedIndex].id
-    let xhr = new XMLHttpRequest()
+let url_estados = 'service/estados/';
 
-    xhr.onload = function() {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            console.log(id);
-            formCities.innerHTML = xhr.responseText
+
+fetch(url_estados).then(response => {
+    response.json().then(estados => {
+        estados.map(estado => {
+            let option = document.createElement("option");
+            option.textContent = `${estado.nome} - (${estado.sigla})`;
+            option.className = 'estado';
+            option.dataset.sigla = estado.sigla;
+            option.id = estado.id;
+            option.value = estado.nome;
+            estados_select.appendChild(option);
+        });    
+    });
+});
+
+estados_select.addEventListener('change', event => {
+    let estados = document.querySelectorAll('.estado');
+    estados.forEach(estado => {
+        if (estado.value === event.target.value) {
+            let url_cidades = `service/cidades/?id=${estado.id}`;
+            console.log(url_cidades);
+            fetch(url_cidades).then(response => {
+                console.log(response);
+                response.json().then(cidades => {
+                    cidades.map(cidade => {
+                        let option = document.createElement("option");
+                        option.textContent = `${cidade.nome}`;
+                        option.className = 'cidade';
+                        option.id = cidade.id;
+                        option.value = cidade.nome;
+                        cidades_select.appendChild(option);
+                        cidades_select.disabled = false;
+                    });
+                })
+            })
         }
-    }
-
-    xhr.open('GET', "class/STATE_ID.php?q=" + id)
-    xhr.send()
-}
+    });
+});
 
